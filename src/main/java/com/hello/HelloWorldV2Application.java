af751c0bb84dd5b0f40a9ce3a3d17c7e6f85581b
joshuaheineindustrieit <60211323+joshuaheineindustrieit@users.noreply.github.com>
2020-01-24 10:19:22 +0800

    Add files via upload

---
+++ src/main/java/com/hello/HelloWorldV2Application.java
@@ -0,0 +1,8 @@
+package com.hello;
+
+public class HelloWorldV2Application {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World this is the maven version of it");
+   }
+}
